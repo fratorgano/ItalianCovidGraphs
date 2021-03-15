fbdae97110c1f079a5fc7fc0268a494aa24f6c64
fratorgano <francesco.torgano@outlook.it>
2021-03-15 19:53:26 +0100

    Added percentage variation in mini plots

--- sample.py
+++ sample.py
@@ -15,7 +15,7 @@ class Sample:
         self.dates = pd.date_range(start=starting_date, periods=len(self.data))
         self.daily_variation = list()
         self.daily_variation.append(0)
-        
+        """Calcualting the difference between the current number of x and the last day number of x"""
         for index, d in enumerate(self.data[1:]):
             if(fix_zero):
                 self.daily_variation.append(d-self.data[index] if d-self.data[index]>0 else 0)
@@ -25,12 +25,12 @@ class Sample:
     
     def line_plot(self, linewidth=2, linestyle='-', marker='o', title=False):
         if(title):
-            plt.title(f'Number of {self.name} (Total: {self.data.iloc[-1]})')
-        
+                percentage_variation = 100*((self.data.iloc[-1]-self.data.iloc[-2])/(self.data.iloc[-1]+self.data.iloc[-2])/2)
+                plt.title(f'Number of {self.name} (Total: {self.data.iloc[-1]}, {percentage_variation:+.2f}%)')
         plt.plot_date(self.dates, self.data, color=self.color, label=self.name, linewidth=linewidth, linestyle=linestyle, marker=marker)
     
-    def variation_bar_plot(self, title='False'):
-        """Calcualting the difference between the current number of x and the last day number of x"""
+    def variation_bar_plot(self, title=False):
         plt.bar(self.dates, self.daily_variation, color=self.color, align='edge', zorder=2, width=1)
+        percentage_variation = 100*((self.daily_variation[-1]-self.daily_variation[-2])/(self.daily_variation[-1]+self.daily_variation[-2])/2)
         if(title):
-            plt.title(f'Daily {self.name} variation (Today: {self.daily_variation[-1]})')
+            plt.title(f'Daily {self.name} variation (Today: {self.daily_variation[-1]}, {percentage_variation:+.2f}%)')
